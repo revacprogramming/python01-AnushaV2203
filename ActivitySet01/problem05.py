3b35ddfd2e52fa88f0bdc33a2e10816d8294dec4
Anu Anusha <97090093+AnushaV2203@users.noreply.github.com>
2022-05-31 09:57:12 +0000

    completed

--- ActivitySet01/problem05.py
+++ ActivitySet01/problem05.py
@@ -1,12 +1,26 @@
-# Functions
+score = 0.0
 
+grade =""
 
-def computepay(h, r):
-    pass  # ...
-
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
-print("Pay", p)
+input1 = input('enter score: ')
+try:score = float(input1)
+except ValueError:
+    print('Bad score')
+    quit()
+     
+if 0.0 <= score <= 1.0:
+    
+    if score >= 0.9:
+        grade = 'A'
+    elif score >= 0.8:
+        grade = 'B'
+    elif score >= 0.7:
+        grade = 'C'
+    elif score >= 0.6:
+        grade = 'D'
+    else:
+        grade = 'F'
+else:
+    grade = 'Bad score'
+    
+print(grade)    
